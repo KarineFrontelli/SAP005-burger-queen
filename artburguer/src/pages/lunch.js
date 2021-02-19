e13d7c4ff3d298e0042ae6d83753a559814ceb04
karinefrontelli <karinefrontellidossantos@gmail.com>
2021-02-18 21:33:13 -0300

    Puxando cardapio da API

--- artburguer/src/pages/lunch.js
+++ artburguer/src/pages/lunch.js
@@ -1,25 +1,21 @@
 import React, { useState } from "react";
 
+const token = localStorage.getItem("token");
 
-
-const token = localStorage.getItem('token');
-
-const MenuLunch= () => {
-  return (
-    fetch("https://lab-api-bq.herokuapp.com/products", {
-        method: "GET",
-        headers: {
-          accept: "application/json",
-          Authorization: `${token}`
-        }
-    })
+const MenuLunch = () => {
+  return fetch("https://lab-api-bq.herokuapp.com/products", {
+    method: "GET",
+    headers: {
+      accept: "application/json",
+      Authorization: `${token}`,
+    },
+  })
     .then((response) => response.json())
     .then((json) => {
-        const lunch = json.filter ((item) => item.type === 'all-day')
-        
+      const lunch = json.filter((item) => item.type === "all-day");
+
       console.log(lunch);
-    })  
-  );
-}
+    });
+};
 
 export default MenuLunch;
