7f9c911062d3b9cdc28c1749db8459c037c144a0
karinefrontelli <karinefrontellidossantos@gmail.com>
2021-02-19 11:04:54 -0300

    Cardapio All-day na tela

--- artburguer/src/pages/breakfast.js
+++ artburguer/src/pages/breakfast.js
@@ -1,4 +1,3 @@
-import { render } from "@testing-library/react";
 import React, { useEffect, useState } from "react";
 import { Link, useHistory } from "react-router-dom";
 import Header from "../Components/Header";
--- artburguer/src/pages/lunch.js
+++ artburguer/src/pages/lunch.js
@@ -1,21 +1,67 @@
-import React, { useState } from "react";
+// import React, { useState } from "react";
 
-const token = localStorage.getItem("token");
+// const token = localStorage.getItem("token");
 
-const MenuLunch = () => {
-  return fetch("https://lab-api-bq.herokuapp.com/products", {
-    method: "GET",
-    headers: {
-      accept: "application/json",
-      Authorization: `${token}`,
-    },
-  })
-    .then((response) => response.json())
-    .then((json) => {
-      const lunch = json.filter((item) => item.type === "all-day");
+// const MenuLunch = () => {
+//   return fetch("https://lab-api-bq.herokuapp.com/products", {
+//     method: "GET",
+//     headers: {
+//       accept: "application/json",
+//       Authorization: `${token}`,
+//     },
+//   })
+//     .then((response) => response.json())
+//     .then((json) => {
+//       const lunch = json.filter((item) => item.type === "all-day");
 
-      console.log(lunch);
-    });
-};
+//       console.log(lunch);
+//     });
+// };
 
+// export default MenuLunch;
+
+import React, { useEffect, useState } from "react";
+import { Link, useHistory } from "react-router-dom";
+import Header from "../Components/Header";
+
+function MenuLunch() {
+  const token = localStorage.getItem("token");
+  const [lunch, setLunch] = useState("");
+
+  useEffect(() => {
+    fetch("https://lab-api-bq.herokuapp.com/products", {
+      method: "GET",
+      headers: {
+        accept: "application/json",
+        Authorization: `${token}`,
+      },
+    })
+      .then((response) => response.json())
+      .then((json) => {
+        const lunch = json.filter((item) => item.type === "all-day");
+        setLunch(lunch);
+        console.log(lunch);
+      });
+  }, []);
+
+  return (
+    <div className="container-all-day">
+      <Header />
+
+      <div className="container-cardapio">
+        <div>
+          <h1>Lanches e Bebidas</h1>
+        </div>
+
+        {lunch &&
+          lunch.map((item) => (
+            <div className="container-itens-lunch">
+              <h2>{item.name}</h2>
+              <h2>R${item.price},00</h2>
+            </div>
+          ))}
+      </div>
+    </div>
+  );
+}
 export default MenuLunch;
