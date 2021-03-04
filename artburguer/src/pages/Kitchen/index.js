63ae19a178b4c68179446c8d5cfb048ac9e2864d
karinefrontelli <karinefrontellidossantos@gmail.com>
2021-03-04 19:07:56 -0300

    Mudança de status e delete da comanda

--- artburguer/src/pages/Kitchen/index.js
+++ artburguer/src/pages/Kitchen/index.js
@@ -1,11 +1,13 @@
 import { useEffect, useState } from "react";
 import Header from "../../Components/Header";
+import { Link, useHistory } from "react-router-dom";
 
 function Cozinha() {
   const token = localStorage.getItem("token");
   const [cozinha, setCozinha] = useState("");
   const nomeCliente = sessionStorage.getItem("cliente");
   const numeroMesa = sessionStorage.getItem("mesa");
+  const rota = useHistory();
 
   useEffect(() => {
     fetch("https://lab-api-bq.herokuapp.com/orders", {
@@ -24,6 +26,7 @@ function Cozinha() {
         const cozinha = json.filter((item) => item.status === "pending");
         setCozinha(cozinha);
         console.log(cozinha);
+        rota.push("/cozinha");
       });
   }, []);
 
@@ -50,45 +53,43 @@ function Cozinha() {
       }),
     })
       .then((response) => response.json())
-      .then((json) => {
-        console.log(json);
-      });
+      .then((json) => {});
   };
 
   return (
     <>
-    <div>
-      <Header />
-    </div>
-    <section className="container-cozinha">
-      {cozinha &&
-        cozinha.map((item, index) => (
-          <div id={item.id} className="container-itens" key={index}>
-            <h2 key={index}>{item.client_name}</h2>
-            <h2 key={index}>{item.table}</h2>
-            <h2 key={index}>{item.id}</h2>
+      <div>
+        <Header />
+      </div>
+      <section className="container-cozinha">
+        {cozinha &&
+          cozinha.map((item, index) => (
+            <div id={item.id} className="container-itens" key={index}>
+              <h2 key={index}>{item.client_name}</h2>
+              <h2 key={index}>{item.table}</h2>
+              <h2 key={index}>{item.id}</h2>
 
-            <h2>
-              {item.status}
-              {Intl.NumberFormat("pt-BR", {
-                style: "currency",
-                currency: "BRL",
-              }).format(item.price)}
-            </h2>
-            <div>
-              produtos
-              {item.Products.map((Products) => (
-                <h1>
-                  {Products.name} {Products.flavor} {Products.complement}
-                </h1>
-              ))}
-              <button type="submit" onClick={handleReady}>
-                Pronto
-              </button>
+              <h2>
+                {item.status}
+                {Intl.NumberFormat("pt-BR", {
+                  style: "currency",
+                  currency: "BRL",
+                }).format(item.price)}
+              </h2>
+              <div>
+                produtos
+                {item.Products.map((Products) => (
+                  <h1>
+                    {Products.name} {Products.flavor} {Products.complement}
+                  </h1>
+                ))}
+                <button type="submit" onClick={handleReady}>
+                  Pronto
+                </button>
+              </div>
             </div>
-          </div>
-        ))}
-    </section>
+          ))}
+      </section>
     </>
   );
 }
